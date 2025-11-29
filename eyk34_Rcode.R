6f502464310ae148525a19df721c75d73e385d54
Edward <edward42105@gmail.com>
2025-11-28 22:16:39 -0500

    second section

--- eyk34_Rcode.R
+++ eyk34_Rcode.R
@@ -1,9 +1,13 @@
+
+                #----Section 1------
 #load the spotify dataset
+
 spotify <- read.csv("dataset.csv")
 
 head(spotify)
 
-#new variabble
+
+#new variable
 spotify$EnergyDanceIndex <- spotify$energy * spotify$danceability
 
 summary(spotify$popularity)
@@ -29,3 +33,27 @@ tapply(spotify$popularity, spotify$track_genre, mean, na.rm = TRUE)[1:10]
 
 #average energy by explicit/non-explicit
 tapply(spotify$energy, spotify$explicit, mean)
+
+                  #------ Section 2-------
+#Histogram of Popularity(numeric)
+hist(
+  spotify$popularity,
+  main = "Distribution of Track Popularity",
+  xlab = "Popularity",
+  breaks = 30
+)
+
+#Barplot of Explicit vs Clean Tracks (categorical)
+explicit_counts <- table(spotify$explicit)
+
+barplot(
+  explicit_counts,
+  main = "Explicit vs Non-Explicit Tracks",
+  xlab = "Explicit (0 = Clean, 1 = Explicit)",
+  ylab = "Count"
+)
+#values of each plot
+breaks <- seq(0, 100, by = 5)
+table(cut(spotify$popularity, breaks, include.lowest = TRUE, right = TRUE))
+table(spotify$explicit)
+
